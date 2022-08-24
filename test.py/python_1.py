2a7f055a7bf314e49376220a509e99111c9d5435
chufuyun <925040155@qq.com>
2022-08-24 20:48:38 +0800

    first commit

---
+++ test.py/python_1.py
@@ -0,0 +1,29 @@
+# _*_ coding: utf-8 _*_
+# @Time: 2022/8/19 10:02
+# @Author:Chu_Fuyun
+# @File:python_1
+# @Project：test.py
+
+# a = [1,2,3,4,'abc','231','d']
+# print(a[-1])
+# print(a[-1:])
+# print(a[:-2])
+
+# dic2 = {'a': 2, 'b': 3, 'c':[2,[2,3,4],4]}
+#
+# dic1 = {'age' : 18, 'height': [2,3,4] , 'minus': 'none', 'contain': dic2 }
+# print(dic2['c'][1][2])
+# print(dic1['contain']['c'])
+#
+# a = 'abas'
+# print(a*3)
+
+## 多变量赋值
+# a, b, c = 1, 2, 3
+
+a = 34
+# a > 10 and print('hello world')
+# a < 10 and print('hello world')
+a >10 or print('你好世界')
+a <10 or print('你好世界')
+ 
